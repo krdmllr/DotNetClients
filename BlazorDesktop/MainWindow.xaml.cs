7e17733a9cd2db364cf8e042085f879f63cb33e3
Konrad Müller <k.l.mueller@outlook.com>
2022-03-02 23:30:23 +0100

    Add sample projects

---
+++ BlazorDesktop/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using BlazorDesktop.Data;
+using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+
+namespace BlazorDesktop {
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddBlazorWebView();
+            serviceCollection.AddSingleton<WeatherForecastService>();
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
+            InitializeComponent();
+        }
+    }
+}
---
+++ XamarinForms/XamarinForms/XamarinForms/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using XamarinForms.Models;
+using XamarinForms.ViewModels;
+
+namespace XamarinForms.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
